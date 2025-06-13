c355a994021153f44a73087fd29b9d149bc70557
peter <peter.kufuwei@gmail.com>
2025-06-13 11:59:30 +0800

    some trivial update

--- Bullet/Bullet8.cpp
+++ Bullet/Bullet8.cpp
@@ -21,7 +21,7 @@ void Bullet8::OnExplode(Enemy *enemy) {
     std::random_device dev;
     std::mt19937 rng(dev());
     std::uniform_int_distribution<std::mt19937::result_type> dist(2, 5);
-    getPlayScene()->GroundEffectGroup->AddNewObject(new DirtyEffect("play/dirty-1.png", dist(rng), enemy->Position.x, enemy->Position.y));
+    //getPlayScene()->GroundEffectGroup->AddNewObject(new DirtyEffect("play/dirty-1.png", dist(rng), enemy->Position.x, enemy->Position.y));
 }
 
 
